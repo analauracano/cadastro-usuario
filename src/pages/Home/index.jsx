21ea662def5898c411c2e85a4f8ac53c6299fb2d
analauracano <laura.fioratti@outlook.com>
2024-08-01 19:35:42 +0100

    criação de api e instalação do cors

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,22 @@
+import { useRef } from 'react'
+import api from '../../services/api'
+
 import { Button, Container, ContainerInputs, Form, Input, InputLabel, Title, TopBackground } from './styles'
 
 import UsersImage from '../../assets/users.png'
 
 function Home() {
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  async function registerNewUser() {
+    await api.post('./usuarios', {
+      email: inputEmail.current.value,
+      age: parseInt(inputAge.current.value),
+      name: inputName.current.value
+    })
+  }
 
   return (
     <Container>
@@ -14,24 +28,24 @@ function Home() {
       <Form>
         <Title>Cadastrar Usuário</Title>
         <ContainerInputs>
-            <div>
-              <InputLabel>Nome<span> *</span></InputLabel>
-              <Input type='text' placeholder='Nome do Usuário' />
-            </div>
+          <div>
+            <InputLabel>Nome<span> *</span></InputLabel>
+            <Input type='text' placeholder='Nome do Usuário' ref={inputName} />
+          </div>
 
-            <div>
-              <InputLabel>Idade<span> *</span></InputLabel>
-              <Input type='number' placeholder='Idade do Usuário' />
-            </div>
+          <div>
+            <InputLabel>Idade<span> *</span></InputLabel>
+            <Input type='number' placeholder='Idade do Usuário' ref={inputAge} />
+          </div>
 
         </ContainerInputs>
 
-        <div style={{width:'100%'}}>
+        <div style={{ width: '100%' }}>
           <InputLabel>E-mail<span> *</span></InputLabel>
-          <Input type='email' placeholder='E-mail do Usuário' />
+          <Input type='email' placeholder='E-mail do Usuário' ref={inputEmail} />
         </div>
 
-        <Button>Cadastrar Usuário</Button>
+        <Button type='button' onClick={registerNewUser}>Cadastrar Usuário</Button>
       </Form>
     </Container>
   )
